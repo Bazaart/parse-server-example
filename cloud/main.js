a6584547f6abde148421e0c9ec90969f966de267
Stas Goferman <stasix@gmail.com>
2016-06-15 17:29:33 +0300

    cloud code - wip

--- cloud/main.js
+++ cloud/main.js
@@ -7,7 +7,7 @@ Parse.Cloud.define('featuredCollections', function(request, response) {
   var query = new Parse.Query("Collections");
   // query.equalTo("isActive", "true")
   // query.equalTo("movie", request.params.movie);
-  query.find({
+  query.get({
     success: function(results) {
       // for (var i = 0; i < results.length; ++i) {
       //   // sum += results[i].get("stars");
