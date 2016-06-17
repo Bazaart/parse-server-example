05404127fe5eb4c2350e8613baeb7bc1391750b5
Stas Goferman <stasix@gmail.com>
2016-06-17 07:56:52 +0300

    added afterDelete for product - pattern delete, and remove product from collections

--- cloud/main.js
+++ cloud/main.js
@@ -3,19 +3,37 @@ Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
 });
 
-Parse.Cloud.define('featuredCollections', function(request, response) {
-  var query = new Parse.Query("Collections");
-  // query.equalTo("isActive", "true")
-  // query.equalTo("movie", request.params.movie);
-  query.get({
-    success: function(results) {
-      // for (var i = 0; i < results.length; ++i) {
-      //   // sum += results[i].get("stars");
-      // }
-      response.success("Stas");
+Parse.Cloud.afterDelete("Products", function (request) {
+
+  // Delete pattern
+  var pattern = request.object.get("pattern")
+  pattern.destroy({
+    success: function (pattern) {
+      console.log('pattern deleted');
+      // The object was deleted from the Parse Cloud.
     },
-    error: function() {
-      response.error("no collections found");
+    error: function (pattern, error) {
+      console.log('pattern could not be deleted');
+      // The delete failed.
+      // error is a Parse.Error with an error code and message.
     }
   });
+
+  // Remove product from all collections
+
+  var query = new Parse.Query("Collections");
+  query.equalTo("products", request.object);
+  query.find({
+    success: function(results) {
+      console.log('removing product from collections')
+      for (var i=0; i < results.length; i++) {
+        results[i].remove("products", request.object)
+        results[i].save()
+      }
+    },
+    error: function(error) {
+      console.log('product was not found in any collection')
+    }
+  })
+
 });
